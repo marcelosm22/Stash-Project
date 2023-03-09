f4a23c3de08b2766dca6cee19642b07aa9918789
Ruda Mafra <rudamafra@gmail.com>
2023-03-09 17:42:04 -0300

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+require "date"
+
+15.times do
+  Forum.create(
+    title: Faker::Movie.title
+  )
+end
+
+User.create(
+  username: "antonio",
+  password: "123456",
+  email: "antonio@gmail.com"
+)
+User.create(
+  username: "marcelo",
+  password: "123456",
+  email: "marcelo@gmail.com"
+)
+User.create(
+  username: "wagner",
+  password: "123456",
+  email: "wagner@gmail.com"
+)
+User.create(
+  username: "ruda",
+  password: "123456",
+  email: "ruda@gmail.com"
+)
+
+10.times do
+  List.create(
+    title: Faker::Color.color_name,
+    user_id: rand(1..4)
+)
+end
+
+10.times do
+  Bookmark.create(
+    list_id: rand(1..10),
+    forum_id: rand(1..15)
+)
+end
+
+10.times do
+  Post.create(
+    content: Faker::Quotes::Shakespeare.romeo_and_juliet_quote,
+    user_id: rand(1..4),
+    date: DateTime.now,
+    forum_id: rand(1..15)
+)
+end
+
+20.times do
+  Comment.create(
+    comment_content: Faker::Quotes::Shakespeare.hamlet_quote,
+    user_id: rand(1..4),
+    post_id: rand(1..10)
+  )
+end
