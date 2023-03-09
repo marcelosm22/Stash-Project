8120c05c3d85b5d9d3f44a0fcba1360ef08446fd
Ruda Mafra <rudamafra@gmail.com>
2023-03-09 16:35:36 -0300

    validations

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,2 +1,7 @@
 class Forum < ApplicationRecord
+  has_many :users
+  has_many :posts
+  has_many :ratings
+  has_many :comments, through: :posts
+  validates :title, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
   belongs_to :user
+  has_many :forums, through: :bookmarks, dependent: :destroy
+  validates :title, presence: true
 end
