0528f2f021b062d6b30c9a72666507005db6b6bc
Wagner Wieland <wagnerp5@hotmail.com>
2023-03-10 11:36:15 -0300

    post-controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.forum = @forum
+    if @post.save
+      redirect_to forum_post(@forum)
+    else
+      render :new, status: :unprocessable_entity
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to_ forum_post(@post.forum), status: :see_other
+  end
+end
+
+  private
+
+  def set_post
+    @forum = Forum.find(params[:forum_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
 end
